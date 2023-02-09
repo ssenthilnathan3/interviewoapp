19c73d02e8917754f79ca5f584c1c0e523a151a9
Senthilnathan <ssenthil1490@outlook.com>
2023-02-09 18:00:47 +0530

    added login page

--- lib/screens/auth/LoginPage.dart
+++ lib/screens/auth/LoginPage.dart
@@ -1,167 +1,178 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:interviewo/utils/constants.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+  LoginScreen({Key? key}) : super(key: key);
+
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _password = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     //Get the size in LoginHeaderWidget()
-    return Container(
-        constraints: const BoxConstraints.expand(),
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                opacity: 0.4,
-                image: AssetImage("assets/cat.jpg"),
-                fit: BoxFit.cover)),
-        child: Scaffold(
-          backgroundColor: Colors.transparent.withOpacity(0),
-          body: SingleChildScrollView(
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
                 children: [
-                  Column(
-                    children: [
-                      // Image(
-                      //     image: const AssetImage('assets/cat.jpg'),
-                      //     height: MediaQuery.of(context).size.height * 0.2),
-                      SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.2),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 30.0),
-                        child: Text("Login",
-                            style: TextStyle(
-                                fontSize: 50,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white)),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 20.0),
-                        child: Text("For You, By Us",
-                            style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white)),
-                      ),
-                    ],
+                  // Image(
+                  //     image: const AssetImage('assets/cat.jpg'),
+                  //     height: MediaQuery.of(context).size.height * 0.2),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: Text("Login",
+                        style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                            color: IOTheme().IOBlue)),
                   ),
-                  Form(
-                    child: Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          TextFormField(
-                            cursorColor: Colors.white,
-                            decoration: const InputDecoration(
-                              enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white)),
-                              prefixIcon: Icon(Icons.person_outline_outlined),
-                              labelText: "Enter your Email",
-                              hintText: "Email Address",
-                              floatingLabelStyle:
-                                  TextStyle(color: Colors.white),
-                              prefixIconColor: Colors.white,
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white)),
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white)),
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          TextFormField(
-                            enabled: true,
-                            decoration: const InputDecoration(
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white)),
-                              prefixIcon: Icon(Icons.fingerprint),
-                              labelText: "Enter your password",
-                              hintText: "Password",
-                              floatingLabelStyle:
-                                  TextStyle(color: Colors.white),
-                              prefixIconColor: Colors.white,
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      width: 10, color: Colors.white)),
-                              suffixIcon: IconButton(
-                                onPressed: null,
-                                icon: Icon(Icons.remove_red_eye_sharp),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: TextButton(
-                                onPressed: () {},
-                                child: const Text("Forgot Password?",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold))),
-                          ),
-                          SizedBox(
-                            width: double.infinity,
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  fixedSize: Size(
-                                      MediaQuery.of(context).size.width * 0.2,
-                                      50),
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20)),
-                                  backgroundColor: Colors.grey),
-                              onPressed: () {},
-                              child: Text("LOGIN",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text("OR",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white)),
-                      const SizedBox(height: 10),
-                      SizedBox(
-                        width: double.infinity,
-                        child: OutlinedButton.icon(
-                          icon: const Image(
-                              image: NetworkImage(
-                                  'https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png&imgrefurl=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F7123025%2Flogo_google_g_icon&tbnid=ZG2Cl8JppQqAmM&vet=12ahUKEwjO9a_U94X9AhUc-jgGHSWFBhYQMygDegUIARDlAQ..i&docid=SCCOGzKGpUtQ9M&w=512&h=512&q=google%20icons&ved=2ahUKEwjO9a_U94X9AhUc-jgGHSWFBhYQMygDegUIARDlAQ'),
-                              width: 20.0),
-                          onPressed: () {},
-                          label: const Text("Signin With Google"),
-                        ),
-                      ),
-                      const SizedBox(height: 50 - 20),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text.rich(
-                          TextSpan(
-                              text: "Don't have an account?",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20),
-                              children: const [
-                                TextSpan(
-                                    text: "Signup",
-                                    style: TextStyle(color: Colors.blue))
-                              ]),
-                        ),
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: Text("For You, By Us",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white)),
                   ),
                 ],
               ),
-            ),
+              Form(
+                key: _formKey,
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextFormField(
+                        controller: _name,
+                        cursorColor: Colors.white,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: IOTheme().IOGreen)),
+                          prefixIcon: Icon(Icons.email_outlined),
+                          labelText: "Enter your Email",
+                          floatingLabelStyle:
+                              TextStyle(color: IOTheme().IOGreen),
+                          prefixIconColor: IOTheme().IOGreen,
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: IOTheme().IOGreen)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: IOTheme().IOGreen)),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: _password,
+                        obscureText: true,
+                        cursorColor: Colors.white,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: IOTheme().IOGreen)),
+                          prefixIcon: Icon(Icons.password_outlined),
+                          labelText: "Enter your Password",
+                          floatingLabelStyle:
+                              TextStyle(color: IOTheme().IOGreen),
+                          prefixIconColor: IOTheme().IOBlue,
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: IOTheme().IOGreen)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: IOTheme().IOGreen)),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                            onPressed: () {},
+                            child: Text("Forgot Password?",
+                                style: TextStyle(
+                                    color: IOTheme().IOBlue,
+                                    fontWeight: FontWeight.bold))),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              fixedSize: Size(
+                                  MediaQuery.of(context).size.width * 0.2, 50),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              backgroundColor:
+                                  IOTheme().IOBlue.withOpacity(0.5)),
+                          onPressed: () {},
+                          child: Text("LOGIN",
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("OR",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: IOTheme().IOBlue)),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: OutlinedButton.icon(
+                        icon: const Image(
+                            image: AssetImage("assets/google.png"), height: 20),
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                            fixedSize: Size(
+                                MediaQuery.of(context).size.width * 0.2, 50),
+                            backgroundColor: IOTheme().IOBlue.withOpacity(0.5),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20))),
+                        label: const Text("Signin With Google",
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text.rich(
+                      TextSpan(
+                          text: "Don't have an account?",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Color.fromARGB(142, 255, 255, 255)),
+                          children: const [
+                            TextSpan(
+                                text: " \tSignup",
+                                style: TextStyle(color: Colors.white))
+                          ]),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class IOTheme {
+  Color IOBlue = Color(0xff032D43);
+  Color IOGreen = Color(0xff86ED78);
+}
